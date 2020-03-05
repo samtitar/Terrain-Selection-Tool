6611c7168415bbece5e644ed8e93df1fdaf27320
Sam Titarsolej <s.titarsolej@gmail.com>
2020-03-05 21:17:41 +0100

    fixed lat/lng issues, added download to folder

--- __init__.py
+++ __init__.py
@@ -30,16 +30,16 @@ def start_download_api():
     if request.args.get("terrain") is None:
         return jsonify({ "error": 2 })
 
-    lat_lng_a = (float(request.args.get("lat_a")), float(request.args.get("lng_a")))
-    lat_lng_b = (float(request.args.get("lat_b")), float(request.args.get("lng_b")))
+    point_a = { "Lat": float(request.args.get("lat_a")), "Lng": float(request.args.get("lng_a")) }
+    point_b = { "Lat": float(request.args.get("lat_b")), "Lng": float(request.args.get("lng_b")) }
 
     name = request.args.get("name")
     terrain = request.args.get("terrain")
 
-    if None in lat_lng_a or None in lat_lng_b:
-        return jsonify({ "error": 3 })
-    
-    map_downloader.add_to_queue(name, terrain, lat_lng_a, lat_lng_b)
+    # if None in lat_lng_a or None in lat_lng_b:
+    #     return jsonify({ "error": 3 })
+
+    map_downloader.add_to_queue(name, terrain, point_a, point_b)
 
     return jsonify({ "success": 1 })
 
--- mapdownloader.py
+++ mapdownloader.py
@@ -1,6 +1,10 @@
 from datetime import datetime
+from threading import Thread
 
 import math
+import requests
+import zipfile
+import io
 
 EARTH_RADIUS = 6371
 
@@ -10,8 +14,8 @@ class MapDownloader:
         self._queue = []
     
     # Add a map to the download queue
-    def add_to_queue(self, name, terrain_type, lat_lng_a, lat_lng_b):
-        download = Download(len(self._queue), name, terrain_type, lat_lng_a, lat_lng_a)
+    def add_to_queue(self, name, terrain_type, point_a, point_b):
+        download = Download(len(self._queue), name, terrain_type, point_a, point_b)
         download.start_download()
         self._queue.append(download)
     
@@ -20,42 +24,104 @@ class MapDownloader:
         return [x.to_dict() for x in self._queue]
     
     def get_download_by_id(self, id):
-        if id < len(self._queue): 
-            return self._queue[id].get_chunks()
+        if id < len(self._queue) and id >= 0:
+            result = self._queue[id].to_dict()
+            result["chunks"] = self._queue[id].get_chunks()
+            result["bounds"] = self._queue[id].get_bounds()
+
+            return result
         return None
 
 class Download:
 
-    def __init__(self, ident, name, terrain_type, lat_lng_a, lat_lng_b):
+    def __init__(self, ident, name, terrain_type, point_a, point_b):
         timestamp = datetime.now()
+
+        final_a = { "Lat": point_a["Lat"], "Lng": point_a["Lng"] }
+        final_b = { "Lat": point_b["Lat"], "Lng": point_b["Lng"] }
+
+        # Swap boundary latitudes if necessary
+        if point_b["Lat"] > point_a["Lat"]:
+            final_a["Lat"] = point_b["Lat"]
+            final_b["Lat"] = point_a["Lat"]
+        
+        # Swap boundary longitudes if necessary
+        if point_b["Lng"] < point_a["Lng"]:
+            final_a["Lng"] = point_b["Lng"]
+            final_b["Lng"] = point_a["Lng"]
 
         self._ident = ident
         self._name = name
-        self._progress = 56
+        self._progress = 0
         self._start = timestamp.strftime("%B %d %H:%M:%S")
         self._terrain = terrain_type
-        self._lat_lng_a = lat_lng_a
-        self._lat_lng_b = lat_lng_b
+        self._point_a = final_a
+        self._point_b = final_b
         self._chunks = []
+
+        self.calculate_chunks()
     
     # Calculate the coordinates of chunks to download
     def calculate_chunks(self):
-        a_lat = self._lat_lng_a[0] + (7.5 / EARTH_RADIUS) * (180 / math.pi)
-        a_lng = self._lat_lng_a[1]
+        box_radius = 15
 
-        b_lat = self._lat_lng_a[0]
-        b_lng = self._lat_lng_a[1] - (15 / EARTH_RADIUS) * (180 / math.pi) / math.cos(self._lat_lng_a[0] * math.pi/180)
+        max_y = self._point_b["Lat"]
+        max_x = self._point_b["Lng"]
 
-        self._chunks.append(((a_lat, a_lng), (b_lat, b_lng)))
-        self._chunks.append((self._lat_lng_a, (0, 0)))
+        Y = self._point_a["Lat"]
+        while Y > max_y:
+            X = self._point_a["Lng"]
+            while X < max_x:
+                dY = box_radius / EARTH_RADIUS
+                dX = box_radius / (EARTH_RADIUS * math.cos(math.pi * Y / 180))
+
+                a_lat = Y - dY * 180 / math.pi
+                a_lng = X
+
+                b_lat = Y
+                b_lng = X + dX * 180 / math.pi
+
+                self._chunks.append(({ "Lat": a_lat, "Lng": a_lng }, { "Lat": b_lat, "Lng": b_lng }))
+
+                X = X + dX * 180 / math.pi
+            Y = Y - dY * 180 / math.pi
 
     # Start a map download
     def start_download(self):
-        pass
-    
+        Thread(target=self.download_chunks).start()
+
+    def download_chunks(self):
+        file_name = "terrainparty Height Map (ASTER 30m).png"
+        api_url = "https://terrain.party/api/export?box="
+
+        total_len = len(self._chunks)
+        progress = 0
+
+        for (i, chunk) in enumerate(self._chunks):
+            str_point_a = str(chunk[0]["Lng"]) + "," + str(chunk[0]["Lat"])
+            str_point_b = str(chunk[1]["Lng"]) + "," + str(chunk[1]["Lat"])
+            request_url = api_url + str_point_a + "," + str_point_b
+
+            req = requests.get(request_url)
+            with zipfile.ZipFile(io.BytesIO(req.content)) as zf:
+                for file in zf.namelist():
+                    if file == file_name:
+                        target_name = "chunk_" + str(i) + ".png"
+                        target_path = "data/" + self._name + "/" + target_name
+
+                        with open(target_path, "wb") as f:
+                            f.write(zf.read(file))
+
+            progress += 1
+            self._progress = (progress / total_len) * 100
+
     # Get the map chunks
     def get_chunks(self):
         return self._chunks
+    
+    # Get map boundaries
+    def get_bounds(self):
+        return [self._point_a, self._point_b]
     
     # Get the map download in dict format
     def to_dict(self):
