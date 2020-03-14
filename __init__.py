70681b4bfb4b8521ee969005fbeb504cda0cd536
Sam Titarsolej <s.titarsolej@gmail.com>
2020-03-14 14:37:04 +0100

    workers update

--- __init__.py
+++ __init__.py
@@ -1,27 +1,45 @@
 from flask import Flask, render_template, request, redirect, jsonify
 
+import json
+import sys
+
 from mapdownloader import MapDownloader
 
-import json
-
 app = Flask(__name__)
-map_downloader = MapDownloader()
+map_downloader = MapDownloader(max_workers=6)
 
+'''
+-----------------
+      VIEWS
+-----------------
+'''
+
+# Endpoint for index view
 @app.route('/', methods=['GET'])
 def index_view():
     page_content = render_template("pages/index.html")
     return render_template("layouts/default.html", content=page_content)
 
+# Endpoint for downloads view
 @app.route('/downloads', methods=['GET'])
 def downloads_view():
     page_content = render_template("pages/downloads.html")
     return render_template("layouts/default.html", content=page_content)
 
+# Endpoint for specific download view
 @app.route('/download', methods=['GET'])
 def download_view():
     page_content = render_template("pages/download.html")
     return render_template("layouts/default.html", content=page_content)
 
+
+'''
+-----------------
+       API
+-----------------
+'''
+
+# Endpoint for starting a new download given the bounding box
 @app.route('/start_download', methods=['GET'])
 def start_download_api():
     if request.args.get("name") is None:
@@ -43,11 +61,13 @@ def start_download_api():
 
     return jsonify({ "success": 1 })
 
+# Endpoint for getting all downloads info
 @app.route('/get_downloads', methods=['GET'])
 def get_downloads_api():
-    result = map_downloader.get_queue()
+    result = map_downloader.get_downloads()
     return jsonify({ "success": 1, "data": result })
 
+# Enpoint 
 @app.route('/get_download', methods=['GET'])
 def get_download_api():
     if request.args.get("ident") is None:
--- mapdownloader.py
+++ mapdownloader.py
@@ -10,31 +10,55 @@ EARTH_RADIUS = 6371
 
 class MapDownloader:
 
-    def __init__(self):
+    def __init__(self, max_workers=4):
+        self._downloads = []
         self._queue = []
+        self._max_workers = max_workers
+        self._num_active_workers = 0
     
     # Add a map to the download queue
     def add_to_queue(self, name, terrain_type, point_a, point_b):
-        download = Download(len(self._queue), name, terrain_type, point_a, point_b)
-        download.start_download()
-        self._queue.append(download)
+        # Create new download
+        download = Download(len(self._downloads), name, terrain_type,
+                            point_a, point_b, callback=self.download_finished)
+
+        # Make sure there a threads availbale for the new download
+        if self._num_active_workers < self._max_workers:
+            download.start_download()
+            self._num_active_workers += 1
+        else:
+            self._queue.append(download)
+        
+        # Store download
+        self._downloads.append(download)
+    
+    # Callback for mapdownload when finished
+    def download_finished(self, download_id):
+        self._num_active_workers -= 1
+        
+        # Start first download in queue
+        if len(self._queue) > 0:
+            next_download = self._queue.pop(0)
+            next_download.start_download()
+            self._num_active_workers += 1
     
     # Get the current items in the queue
-    def get_queue(self):
-        return [x.to_dict() for x in self._queue]
+    def get_downloads(self):
+        return [x.to_dict() for x in self._downloads]
     
+    # Get -ALL- information on a specific map download
     def get_download_by_id(self, id):
-        if id < len(self._queue) and id >= 0:
-            result = self._queue[id].to_dict()
-            result["chunks"] = self._queue[id].get_chunks()
-            result["bounds"] = self._queue[id].get_bounds()
+        if id < len(self._downloads) and id >= 0:
+            result = self._downloads[id].to_dict()
+            result["chunks"] = self._downloads[id].get_chunks()
+            result["bounds"] = self._downloads[id].get_bounds()
 
             return result
         return None
 
 class Download:
 
-    def __init__(self, ident, name, terrain_type, point_a, point_b):
+    def __init__(self, ident, name, terrain_type, point_a, point_b, callback=None):
         timestamp = datetime.now()
 
         final_a = { "Lat": point_a["Lat"], "Lng": point_a["Lng"] }
@@ -51,27 +75,41 @@ class Download:
             final_b["Lng"] = point_a["Lng"]
 
         self._ident = ident
+
+        # Display data
         self._name = name
         self._progress = 0
+        self._status = 'waiting'
         self._start = timestamp.strftime("%B %d %H:%M:%S")
         self._terrain = terrain_type
+
+        # Chunks data
         self._point_a = final_a
         self._point_b = final_b
         self._chunks = []
+
+        # Thread data
+        self._thread = Thread(target=self.download_chunks, daemon=True)
+        self._callback = callback
 
         self.calculate_chunks()
     
     # Calculate the coordinates of chunks to download
     def calculate_chunks(self):
-        box_radius = 15
+        box_radius = 8
 
         max_y = self._point_b["Lat"]
         max_x = self._point_b["Lng"]
 
         Y = self._point_a["Lat"]
+
+        # Loop over all y coordinates
         while Y > max_y:
             X = self._point_a["Lng"]
+
+            # Loop over all x coordinates
             while X < max_x:
+                # Calculate bounding box
                 dY = box_radius / EARTH_RADIUS
                 dX = box_radius / (EARTH_RADIUS * math.cos(math.pi * Y / 180))
 
@@ -81,6 +119,7 @@ class Download:
                 b_lat = Y
                 b_lng = X + dX * 180 / math.pi
 
+                # Add bounding box to list
                 self._chunks.append(({ "Lat": a_lat, "Lng": a_lng }, { "Lat": b_lat, "Lng": b_lng }))
 
                 X = X + dX * 180 / math.pi
@@ -88,8 +127,15 @@ class Download:
 
     # Start a map download
     def start_download(self):
-        Thread(target=self.download_chunks).start()
+        self._status = 'downloading'
+        self._thread.start()
 
+    # Stop a map download
+    def stop_download(self):
+        self._status = 'done'
+        self._thread.join()
+
+    # Download all chunks
     def download_chunks(self):
         file_name = "terrainparty Height Map (ASTER 30m).png"
         api_url = "https://terrain.party/api/export?box="
@@ -97,23 +143,34 @@ class Download:
         total_len = len(self._chunks)
         progress = 0
 
+        # Loop over all chunks
         for (i, chunk) in enumerate(self._chunks):
             str_point_a = str(chunk[0]["Lng"]) + "," + str(chunk[0]["Lat"])
             str_point_b = str(chunk[1]["Lng"]) + "," + str(chunk[1]["Lat"])
-            request_url = api_url + str_point_a + "," + str_point_b
 
+            # Make request to terrain party api
+            request_url = api_url + str_point_a + "," + str_point_b
             req = requests.get(request_url)
+
+            # Extract downloaded zipfile
             with zipfile.ZipFile(io.BytesIO(req.content)) as zf:
+
+                # Find terrain height map
                 for file in zf.namelist():
                     if file == file_name:
-                        target_name = "chunk_" + str(i) + ".png"
-                        target_path = "data/" + self._name + "/" + target_name
+                        target_name = self._name + "_chunk_" + str(i) + ".png"
+                        target_path = "data/" + self._terrain + "/" + target_name
 
+                        # Extract from zip and save in terrain folder
                         with open(target_path, "wb") as f:
                             f.write(zf.read(file))
-
+            
+            # Update progress
             progress += 1
             self._progress = (progress / total_len) * 100
+        
+        if self._callback:
+            self._callback(self._ident)
 
     # Get the map chunks
     def get_chunks(self):
@@ -125,4 +182,11 @@ class Download:
     
     # Get the map download in dict format
     def to_dict(self):
-        return { "name": self._name, "progress": self._progress, "start": self._start, "terrain": self._terrain }
+        return {
+            "name": self._name,
+            "progress": self._progress,
+            "status": self._status,
+            "start": self._start,
+            "terrain": self._terrain,
+            "num_chunks": len(self._chunks)
+        }
